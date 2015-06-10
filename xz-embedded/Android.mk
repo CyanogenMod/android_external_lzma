cc47a47b77cee783e3f055b515179fd9aa63149d
Steve Kondik <steve@cyngn.com>
2015-06-10 12:55:55 -0700

    xz: Enable crc64 support
     * Needed to decompress mostly anything.
    
    Change-Id: I71b21f0131c5bf9a40ec0bce0ab15b9fc05944da

--- xz-embedded/Android.mk
+++ xz-embedded/Android.mk
@@ -4,6 +4,7 @@ LOCAL_PATH := $(call my-dir)
 
 xz_embedded_sources := \
     xz_crc32.c \
+    xz_crc64.c \
     xz_dec_bcj.c \
     xz_dec_lzma2.c \
     xz_dec_stream.c
@@ -12,6 +13,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libxz
 LOCAL_SRC_FILES := $(xz_embedded_sources)
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+LOCAL_CFLAGS := -DXZ_USE_CRC64 -DXZ_DEC_ANY_CHECK
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
